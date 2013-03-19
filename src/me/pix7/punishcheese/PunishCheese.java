38d94d94ca0a7f12bc1143a99b37691392a25950
pixelart7 <sonkungz_on@hotmail.com>
2013-03-19 14:42:24 +0700

    Added database connect API and create table onEnable

--- src/me/pix7/punishcheese/PunishCheese.java
+++ src/me/pix7/punishcheese/PunishCheese.java
@@ -1,13 +1,32 @@
 package me.pix7.punishcheese;
 
+import java.sql.SQLException;
+
+import me.pix7.punishcheese.database.MySQLAPI;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class PunishCheese extends JavaPlugin{
-
+	
 	public void onEnable(){
 		
 		this.getConfig().set("PunishCheesePrefix", "&4(Punish) &f");
 		getCommand("punishcheese").setExecutor(new me.pix7.punishcheese.commands.CommonCommand(this));
+		getCommand("addrule").setExecutor(new me.pix7.punishcheese.commands.DatabaseCommand(this));
+		getCommand("addruledetail").setExecutor(new me.pix7.punishcheese.commands.DatabaseCommand(this));
+		
+		String username = "root";
+		String password = "pixel";
+		String db = "bukkit";
+		String ip = "localhost";
+		String port = "3306";
+		
+		MySQLAPI sql = new MySQLAPI(username, password, db, ip, port);
+		try {
+			sql.update("CREATE TABLE IF NOT EXISTS `punishcheese_rules` (`ruleid` int(3) NOT NULL, `shortdetail` varchar(256) NOT NULL, `detail` varchar(512) NOT NULL) ENGINE=InnoDB DEFAULT CHARSET=utf8;");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		
 	}
 	
---
+++ src/me/pix7/punishcheese/database/MySQLAPI.java
@@ -0,0 +1,96 @@
+package me.pix7.punishcheese.database;
+
+import java.sql.*;
+/**
+ * @author iSuzutsuki
+ * MySQL Manager Class
+ */
+public class MySQLAPI {
+
+	String username;
+	String password;
+	String db;
+	String port;
+	String ip;
+	/**
+	 * Construct new instance of MySQLAPI using
+	 * @param username - Username to connect
+	 * @param password - password of @param username
+	 * @param db - Database name
+	 * @param ip - Address of MySQL Server
+	 * @param port - Port of MySQL Server.
+	 */
+	public MySQLAPI(String username,String password,String db,String ip,String port)
+	{
+		this.username = username;
+		this.password = password;
+		this.db = db;
+		this.port = port;
+		this.ip = ip;
+	}
+	/**
+	 * Query SELECT query to MySQL Server
+	 * @param query - Query to send
+	 * @return ResultSet of query results
+	 */
+	public ResultSet query(String query){
+		try{			
+			Statement stmt = null;
+			ResultSet rs = null;
+			try {
+    			Class.forName("com.mysql.jdbc.Driver");
+    			
+    		} catch (Exception ex){
+    			ex.printStackTrace();
+    			return null;
+    		}
+    		try {
+    	StringBuilder sqlurl = new StringBuilder();
+    	sqlurl.append("jdbc:mysql://").append(ip).append(":").append(port).append("/").append(db).append("?user=").append(username).append("&password=").append(password);
+    	Connection con = DriverManager.getConnection(sqlurl.toString());
+        stmt = con.createStatement();
+        rs = stmt.executeQuery(query);
+        return rs;
+    			}
+    		catch(Exception e){
+    			e.printStackTrace();
+    			return null;
+    		}
+		}catch(Exception e){
+			e.printStackTrace();
+			return null;
+		}
+	}
+	/**
+	 * Query INSERT UPDATE DELETE or query that doesn't have ResultSet as result
+	 * @param query - query to send
+	 * @return count of affected rows
+	 * @throws SQLException
+	 */
+	public int update(String query) throws SQLException{
+		try{
+			Statement stmt = null;
+			try {
+    			Class.forName("com.mysql.jdbc.Driver");
+    			
+    		} catch (Exception ex){
+    			ex.printStackTrace();
+    			return 0;
+    		}
+    		try {
+    	StringBuilder sqlurl = new StringBuilder();
+    	sqlurl.append("jdbc:mysql://").append(ip).append(":").append(port).append("/").append(db).append("?user=").append(username).append("&password=").append(password);
+    	Connection con = DriverManager.getConnection(sqlurl.toString());
+        stmt = con.createStatement();
+        int i = stmt.executeUpdate(query);
+        return i;
+    		}catch(Exception e){
+    			e.printStackTrace();
+    			throw new SQLException("Couldn't complete query");
+    		}
+		}catch(Exception e){
+			e.printStackTrace();
+			throw new SQLException("Couldn't complete query");
+		}
+	}
+}
